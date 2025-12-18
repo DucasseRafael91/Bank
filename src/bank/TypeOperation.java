20f266118cd674caed8e6dea064b6ffec9ab6a27
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2025-12-18 13:38:30 +0100

    Creation du model TypeOperation

---
+++ src/bank/TypeOperation.java
@@ -0,0 +1,31 @@
+package bank;
+
+public class TypeOperation {
+	
+	private int id;
+	private String nom;
+	
+	 public TypeOperation(String nom) {
+	        this.id = id;
+	        this.nom = nom;
+	    }
+	 
+	 // Getters
+	    public int getId() {
+	        return id;
+	    }
+
+	    public String getNom() {
+	        return nom;
+	    }
+	    
+	    //Setters
+	    public void setId(int id) {
+	        this.id = id;
+	    }
+
+	    public void setNom(String nom) {
+	        this.nom = nom;
+	    }
+
+}
