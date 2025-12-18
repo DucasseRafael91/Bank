6c1f990840f2c5646a3af81c3d8079cf2c25650a
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2025-12-18 13:44:36 +0100

    Creation du model Operation

---
+++ src/bank/Operation.java
@@ -0,0 +1,64 @@
+package bank;
+
+import java.util.Date;
+
+public class Operation {
+    
+    private int id;
+    private int compte;
+    private int type_operation;
+    private double montant;
+    private Date date;
+    
+    public Operation(int compte, int type_operation, double montant, Date date) {
+        this.compte = compte;
+        this.type_operation = type_operation;
+        this.montant = montant;
+        this.date = date;
+    }
+
+    // Getter et Setter pour id
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // Getter et Setter pour compte
+    public int getCompte() {
+        return compte;
+    }
+
+    public void setCompte(int compte) {
+        this.compte = compte;
+    }
+
+    // Getter et Setter pour type_operation
+    public int getType_operation() {
+        return type_operation;
+    }
+
+    public void setType_operation(int type_operation) {
+        this.type_operation = type_operation;
+    }
+
+    // Getter et Setter pour montant
+    public double getMontant() {
+        return montant;
+    }
+
+    public void setMontant(double montant) {
+        this.montant = montant;
+    }
+
+    // Getter et Setter pour date
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
